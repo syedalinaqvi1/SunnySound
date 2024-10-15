7d4e72e131fff1cfca43ffa5add077f4855c6a74
Syed Haider Ali <haiderali378378@gmail.com>
2024-10-15 14:48:26 +0500

    scrolllviewAdd

--- app/testThree.tsx
+++ app/testThree.tsx
@@ -1,5 +1,12 @@
 import React, { useEffect, useRef, useState } from "react";
-import { Image, View, Text, Dimensions, Pressable } from "react-native";
+import {
+  Image,
+  View,
+  Text,
+  Dimensions,
+  Pressable,
+  ScrollView,
+} from "react-native";
 import Animated, {
   withDelay,
   withSpring,
@@ -82,117 +89,123 @@ export default function AnimatedScreen() {
   };
 
   return (
-    <View className="flex-1 items-center w-[100%]  bg-[#F3F7FF]">
-      <View className=" items-center flex-1 w-[100%] h-[100%] ">
-        <Image
-          source={require("./assets/images/logo.png")}
-          className="w-[50%] h-[6%] mt-12 "
-          resizeMode="contain"
-        />
-        <Text className="text-[32px] md:text-3xl text-[#313B4D] text-center mt-2 ">
-          {text}
-        </Text>
-        <View className="bg-white w-[91%] p-2 rounded-[17px] pb-5 mt-4 ">
-          <View className="items-center flex-row px-2 pt-1">
-            <Hexagon
-              hexagonSize={deviceHeight / 22}
-              fillPercentage={1}
-              cornerRadius={2.5}
-              iconWidth={deviceHeight / 22}
-              iconHeight={deviceHeight / 22}
-              strokeWidth={4}
-              activeColor={Trophy_Hexagon.activeColor}
-              fillColor={Trophy_Hexagon.fillColor}
-              borderColor={Trophy_Hexagon.borderColor}
-            />
-            <View className="pl-1">
-              <Text className="text-2xl md:text-2xl font-bold text-[#313B4D]">
-                Training complete
-              </Text>
-              <Text className="text-lg md:text-2xl font-normal text-[#5D687E]">
-                15 minutes - 30 exercises
-              </Text>
-            </View>
-          </View>
-          <View className=" items-center flex-row self-center w-[85%] ">
-            <Progress.Bar
-              progress={progressBar}
-              width={deviceWidth / 1.8}
-              height={progressBarHeight}
-              color={"#0D61FD"}
-              unfilledColor={"white"}
-              borderWidth={1}
-              borderColor="#C9D0DE"
-            />
-            <View className="justify-between items-center flex-row self-center pb-1 w-[26%]">
-              <Text className="text-base md:text-xl text-[#5D687E] w-[30">
-                {` ${(progressBar * 100).toFixed(0)}`} of
-              </Text>
-              <Text className="text-base md:text-xl pl-2 text-[#5D687E]">
-                100
-              </Text>
-            </View>
-          </View>
-          <View>
-            {textData.map((item, index) => {
-              const animatedStyle = useAnimatedStyle(() => {
-                return {
-                  transform: [{ scale: animations[index].value }],
-                };
-              });
-              return (
-                <Animated.View key={index.toString()} style={animatedStyle}>
-                  <TextContainer item={item} />
-                </Animated.View>
-              );
-            })}
-          </View>
-        </View>
-        {showCard && (
-          <Animated.View
-            style={[animatedCardStyle]}
-            className="bg-white w-[91%] p-2 rounded-[17px] mt-4 py-3 px-4 mb-4"
-          >
-            <View className="items-center rounded-[30px] z-10 flex-row ">
-              <Image
-                source={require("./assets/images/flame.png")}
-                style={{ width: 70, height: 120 }}
-                resizeMode="cover"
+    <>
+      <ScrollView
+        contentContainerStyle={{ paddingBottom: 100 }}
+        showsVerticalScrollIndicator={false}
+        className=" w-[100%] bg-[#F3F7FF] "
+      >
+        <View className=" items-center">
+          <Image
+            source={require("./assets/images/logo.png")}
+            className="w-[50%] h-[6%] mt-12 "
+            resizeMode="contain"
+          />
+          <Text className="text-[32px] md:text-3xl text-[#313B4D] text-center mt-2 ">
+            {text}
+          </Text>
+          <View className="bg-white w-[91%] p-2 rounded-[17px] pb-5 mt-4 ">
+            <View className="items-center flex-row px-2 pt-1">
+              <Hexagon
+                hexagonSize={deviceHeight / 22}
+                fillPercentage={1}
+                cornerRadius={2.5}
+                iconWidth={deviceHeight / 22}
+                iconHeight={deviceHeight / 22}
+                strokeWidth={4}
+                activeColor={Trophy_Hexagon.activeColor}
+                fillColor={Trophy_Hexagon.fillColor}
+                borderColor={Trophy_Hexagon.borderColor}
               />
-              <View>
-                <View className=" ml-6">
-                  <Text className="text-2xl md:text-2xl font-semibold text-[#313B4D]">
-                    7 days in a row!
-                  </Text>
-                  <View className="items-center flex-row mt-2 mb-2  ">
-                    <Image
-                      source={require("./assets/images/calendar2.png")}
-                      style={{ width: 35, height: 35, marginLeft: 1 }}
-                      resizeMode="cover"
-                      tintColor={"#0D61FD"}
-                    />
-                    <Image
-                      source={require("./assets/images/line.png")}
-                      style={{ width: 135, height: 50, marginLeft: 2 }}
-                      resizeMode="contain"
-                      tintColor={"#0D61FD"}
-                    />
-                    <Image
-                      source={require("./assets/images/checkmark.png")}
-                      style={{ width: 40, height: 40 }}
-                      resizeMode="cover"
-                      tintColor={"#0D61FD"}
-                    />
-                  </View>
-                  <Text className="text-lg md:text-xl font-normal text-[#5D687E] w-60">
-                    You are on a streak! See ya tomorrow, ok?
-                  </Text>
-                </View>
+              <View className="pl-1">
+                <Text className="text-2xl md:text-2xl font-bold text-[#313B4D]">
+                  Training complete
+                </Text>
+                <Text className="text-lg md:text-2xl font-normal text-[#5D687E]">
+                  15 minutes - 30 exercises
+                </Text>
               </View>
             </View>
-          </Animated.View>
-        )}
-      </View>
+            <View className=" items-center flex-row self-center w-[85%] ">
+              <Progress.Bar
+                progress={progressBar}
+                width={deviceWidth / 1.8}
+                height={progressBarHeight}
+                color={"#0D61FD"}
+                unfilledColor={"white"}
+                borderWidth={1}
+                borderColor="#C9D0DE"
+              />
+              <View className="justify-between items-center flex-row self-center pb-1 w-[26%]">
+                <Text className="text-base md:text-xl text-[#5D687E] w-[30">
+                  {` ${(progressBar * 100).toFixed(0)}`} of
+                </Text>
+                <Text className="text-base md:text-xl pl-2 text-[#5D687E]">
+                  100
+                </Text>
+              </View>
+            </View>
+            <View>
+              {textData.map((item, index) => {
+                const animatedStyle = useAnimatedStyle(() => {
+                  return {
+                    transform: [{ scale: animations[index].value }],
+                  };
+                });
+                return (
+                  <Animated.View key={index.toString()} style={animatedStyle}>
+                    <TextContainer item={item} />
+                  </Animated.View>
+                );
+              })}
+            </View>
+          </View>
+          {showCard && (
+            <Animated.View
+              style={[animatedCardStyle]}
+              className="bg-white w-[91%] p-2 rounded-[17px] mt-4 py-6 px-4 mb-4"
+            >
+              <View className="items-center rounded-[30px] z-10 flex-row ">
+                <Image
+                  source={require("./assets/images/flame.png")}
+                  style={{ width: 70, height: 120 }}
+                  resizeMode="cover"
+                />
+                <View>
+                  <View className=" ml-6">
+                    <Text className="text-2xl md:text-2xl font-semibold text-[#313B4D]">
+                      7 days in a row!
+                    </Text>
+                    <View className="items-center flex-row mt-2 mb-2  ">
+                      <Image
+                        source={require("./assets/images/calendar2.png")}
+                        style={{ width: 35, height: 35, marginLeft: 1 }}
+                        resizeMode="cover"
+                        tintColor={"#0D61FD"}
+                      />
+                      <Image
+                        source={require("./assets/images/line.png")}
+                        style={{ width: 135, height: 50, marginLeft: 2 }}
+                        resizeMode="contain"
+                        tintColor={"#0D61FD"}
+                      />
+                      <Image
+                        source={require("./assets/images/checkmark.png")}
+                        style={{ width: 40, height: 40 }}
+                        resizeMode="cover"
+                        tintColor={"#0D61FD"}
+                      />
+                    </View>
+                    <Text className="text-lg md:text-xl font-normal text-[#5D687E] w-60">
+                      You are on a streak! See ya tomorrow, ok?
+                    </Text>
+                  </View>
+                </View>
+              </View>
+            </Animated.View>
+          )}
+        </View>
+      </ScrollView>
       <View
         className="h-[10%] w-full bg-white items-center justify-evenly"
         style={{
@@ -215,6 +228,6 @@ export default function AnimatedScreen() {
           </Text>
         </Pressable>
       </View>
-    </View>
+    </>
   );
 }
